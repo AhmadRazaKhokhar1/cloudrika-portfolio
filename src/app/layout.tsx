e36e3260f9fdcf7fcf5b5d000e631da8aba4f77b
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-08-29 15:43:13 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
-
+import Footer from "@/components/Footer";
 
 export var metadata: Metadata = {
   title: "Cloud Rika",
-  description: "Cloud Rika - The Best All in One Software Solution for your Business. Get Your ERP, CRM, Email Portal, Civil Operations Portal, Mobile apps, Webapps, Chrome Extensions!",
-  keywords:"Cloud Rika, Cloudrika, cloudrika, cloud, cloudrica, cloud rica, cloud solutions, clouds, rika clouds, Ahamd Raza Khokhar"
+  description:
+    "Cloud Rika - The Best All in One Software Solution for your Business. Get Your ERP, CRM, Email Portal, Civil Operations Portal, Mobile apps, Webapps, Chrome Extensions!",
+  keywords:
+    "Cloud Rika, Cloudrika, cloudrika, cloud, cloudrica, cloud rica, cloud solutions, clouds, rika clouds, Ahamd Raza Khokhar",
 };
 
 export default function RootLayout({
@@ -16,9 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{fontFamily:"cursive"}}>
-        <Navbar/>
-        {children}
+      <body
+        style={{ fontFamily: "cursive" }}
+        className="flex flex-col justify-between "
+      >
+        <Navbar />
+        <main className="flex justify-between items-center">{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
+import Image from "next/image";
 import { Fragment } from "react";
-
+import bannerImage from "../assets/Cloud Rika - Top Banner.webp";
+import excellentSoftwareSolutionSideMiniBanner from "../assets/Coudrika Software Excellence.webp";
 export default function Home() {
   return (
-   <Fragment>
+    <div className="flex flex-col justify-between items-center w-full">
+      <div className="w-full h-[25vh] sm:h-[50vh] md:h-[60vh] lg:h-[80vh] overflow-hidden bg-cover">
+        <Image
+          src={bannerImage}
+          alt="The Best Software Solution For Your Business - CloudRika"
+          className="w-full"
+        />
+      </div>
 
-   </Fragment>
+      <div className="flex flex-wrap w-[100%] p-5 h-full self-start top-0 items-center justify-center">
+        <span className="paragraph text-left w-[50%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+          quam tempore, numquam maiores ullam necessitatibus rerum? Facere
+          nesciunt, voluptates in voluptatibus ex eveniet odio molestiae ad
+          assumenda dolorum aliquid fugit.
+        </span>
+        <span className="overflow-hidden w-[30%] h-[30%] rounded-md shadow-md shadow-black">
+          <Image
+            src={excellentSoftwareSolutionSideMiniBanner}
+            alt="The Best Software Solution For Your Business - CloudRika"
+            className="w-full"
+          />
+        </span>
+      </div>
+      <div className="flex flex-wrap w-[100%] p-5 h-full self-start top-0 items-center justify-center">
+        <span className="overflow-hidden w-[30%] h-[30%] flex-row-reverse rounded-md shadow-md shadow-black">
+          <Image
+            src={excellentSoftwareSolutionSideMiniBanner}
+            alt="The Best Software Solution For Your Business - CloudRika"
+            className="w-full"
+          />
+        </span>
+        <span className="paragraph text-left w-[50%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+          quam tempore, numquam maiores ullam necessitatibus rerum? Facere
+          nesciunt, voluptates in voluptatibus ex eveniet odio molestiae ad
+          assumenda dolorum aliquid fugit.
+        </span>
+      </div>
+    </div>
   );
 }
