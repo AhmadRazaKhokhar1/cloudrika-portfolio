2b53b2203e305187b6c0b47249fb57f2acc05ab7
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-08-30 12:27:10 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 import HeaderBanner from "@/components/HeaderBanner";
-import headerImageMain from '../assets/Cloud Rika Home Banner.webp'
+import headerImageMain from "../assets/Cloud Rika Home Banner.webp";
 export default function Home() {
   return (
-   <div className="flex flex-col justify-between items-center h-full w-full">
+    <div className="flex flex-col justify-between items-center h-full w-full">
       <div className="w-full h-auto">
-      <HeaderBanner 
-      altText="Cloud Rika - The Best All In One Productivity Software Solutions For Your Business"
-      imgSrc={headerImageMain.src}
-      />
-      <button 
-      className="absolute block lg:px-8 text-white lg:py-5 bg-[#1e99d5] right-2 top-32 px-3 py-1 w-32 text-xs lg:text-md lg:w-52 
-      xl:right-12 xl:top-[36rem] shadow-lg shadow-[#2f375c] rounded-md hover:shdow-xl hover:shadow-black hover:scale-105 transition 
+        <HeaderBanner
+          altText="Cloud Rika - The Best All In One Productivity Software Solutions For Your Business"
+          imgSrc={headerImageMain.src}
+        />
+        <button
+          className="absolute block lg:px-8 text-white lg:py-5 bg-[#1e99d5] right-1 top-48 sm:top-80 md:top-96 lg:top-[30rem] px-3 py-1 w-24 text-xs lg:text-md lg:w-52 xl:right-12 xl:top-[36rem] shadow-lg shadow-[#2f375c] rounded-md hover:shdow-xl hover:shadow-black hover:scale-105 transition 
       ease-in-out delay-75"
-      
-      >
-        Schedule a free call
-      </button>
+        >
+          Schedule a free call
+        </button>
       </div>
-   </div>
+    </div>
   );
 }
